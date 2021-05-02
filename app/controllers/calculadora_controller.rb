691832dee99ccc01168a3b43d51fa624a3e093e2
Neil Carvalho <me@neil.pro>
2021-05-02 13:32:35 -0300

    Cálculo executado corretamente

--- app/controllers/calculadora_controller.rb
+++ app/controllers/calculadora_controller.rb
@@ -3,7 +3,17 @@ class CalculadoraController < ApplicationController
   end
 
   def calcular
-    # BC = (Valor do Produto + Valor do IPI + Frete + Seguro + Outras Despesas Acessórias – Descontos) x (1+ (Margem de Valor Agregado/100)
+    # Base de cálculo do ICMS interestadual:
+    # (Valor do Produto + Frete + Seguro + Outras Despesas Acessórias – Descontos)
+    #
+    # Valor do ICMS interestadual:
+    # (Base de cálculo do ICMS interestadual) * (Alíquota de ICMS interestadual / 100)
+    #
+    # Base de cálculo do ICMS ST:
+    # (Valor do Produto + Valor do IPI + Frete + Seguro + Outras Despesas Acessórias – Descontos) x (1+ (Margem de Valor Agregado/100)
+    #
+    # Valor do ICMS ST:
+    # ((Base de cálculo do ICMS ST) * (Alíquota do ICMS interno / 100)) - Valor do ICMS interestadual
     valor_produto = params[:valor_produto].to_f
     valor_ipi = params[:valor_ipi].to_f
     valor_frete = params[:valor_frete].to_f
@@ -11,9 +21,14 @@ class CalculadoraController < ApplicationController
     valor_outras_despesas = params[:valor_outras_despesas].to_f
     valor_descontos = params[:valor_descontos].to_f
     mva = params[:mva].to_f
+    aliquota_operacao = params[:aliquota_operacao].to_f
+    aliquota_interna = params[:aliquota_interna].to_f
 
-    base_de_calculo = (valor_produto + valor_ipi + valor_frete + valor_seguro + valor_outras_despesas - valor_descontos) * (1 + (mva / 100))
+    base_de_calculo_icms = valor_produto + valor_frete + valor_seguro + valor_outras_despesas - valor_descontos
+    base_de_calculo_icms_st = (base_de_calculo_icms + valor_ipi) * (1 + (mva / 100))
+    valor_icms_operacao = base_de_calculo_icms * (aliquota_operacao / 100)
+    valor_icms_st = base_de_calculo_icms_st * (aliquota_interna / 100) - valor_icms_operacao
 
-    render plain: base_de_calculo
+    render plain: valor_icms_st
   end
 end
