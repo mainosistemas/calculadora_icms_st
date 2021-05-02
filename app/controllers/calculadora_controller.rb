5943047f299a0f72213188cda1c1d0f62fcff3fd
Neil Carvalho <me@neil.pro>
2021-05-02 13:01:23 -0300

    Incluídas alíquotas de ICMS

--- app/controllers/calculadora_controller.rb
+++ app/controllers/calculadora_controller.rb
@@ -1,8 +1,19 @@
 class CalculadoraController < ApplicationController
   def index
-    # BC = (Valor do Produto + Valor do IPI + Frete + Seguro + Outras Despesas Acessórias – Descontos) x (1+ (Margem de Valor Agregado/100)
   end
 
   def calcular
+    # BC = (Valor do Produto + Valor do IPI + Frete + Seguro + Outras Despesas Acessórias – Descontos) x (1+ (Margem de Valor Agregado/100)
+    valor_produto = params[:valor_produto].to_f
+    valor_ipi = params[:valor_ipi].to_f
+    valor_frete = params[:valor_frete].to_f
+    valor_seguro = params[:valor_seguro].to_f
+    valor_outras_despesas = params[:valor_outras_despesas].to_f
+    valor_descontos = params[:valor_descontos].to_f
+    mva = params[:mva].to_f
+
+    base_de_calculo = (valor_produto + valor_ipi + valor_frete + valor_seguro + valor_outras_despesas - valor_descontos) * (1 + (mva / 100))
+
+    render plain: base_de_calculo
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "calculadora#index"
   get "calculadora", to: "calculadora#index"
   post "calculadora", to: "calculadora#calcular"
 end
